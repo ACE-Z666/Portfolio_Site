d5ffd57d4a4dbca645da18679761a91e3367022d
Abhijith_J_Nair <136841851+ACE-Z666@users.noreply.github.com>
2025-11-27 20:00:09 +0530

    API Update

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,15 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import .env
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAXnddDUrHfJw61e0gf937Vbg0ZHA528ZI",
-  authDomain: "my-portfolio-41e33.firebaseapp.com",
-  projectId: "my-portfolio-41e33",
-  storageBucket: "my-portfolio-41e33.appspot.com",
-  messagingSenderId: "149995704329",
-  appId: "1:149995704329:web:9852af42b478a49e7b28d8",
-  measurementId: "G-YCWLBNKX8H"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDER,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 };
 
 // Initialize Firebase (ensuring it only runs once)
